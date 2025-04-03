d8192459d8817d3e99936ba0bf23450b84b9ced0
Jalil lalouani <jalil.lalouani@gmail.com>
2025-04-03 20:50:49 +0200

    Suppression de l'appel au constructeur parent dans WishListController pour simplifier l'initialisation de la classe.

--- api/controllers/WishListController.php
+++ api/controllers/WishListController.php
@@ -7,7 +7,6 @@ class WishListController extends BaseController {
     private $wishListModel;
 
     public function __construct() {
-        parent::__construct();
         $this->wishListModel = new WishList();
     }
 
